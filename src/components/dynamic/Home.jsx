bc125aa8c28a8e2b55e350cb072fef8a4bca72be
drjhatka <bishawjit12@gmail.com>
2024-06-25 22:00:25 -0400

    polish and polish it to make it shine!

--- src/components/dynamic/Home.jsx
+++ src/components/dynamic/Home.jsx
@@ -26,10 +26,15 @@ function Home() {
 
     return (
         <div className='w-4/5 mx-auto'>
+            <div className='shadow-md rounded-md bg-[#FF5733]  mb-6 px-4 py-4 text-4xl'>
+                    <h1 className='py-4 border-b-4 text-white   border-b-white  pl-10'>New Arrivals <br /><small>Fresh off the oven!</small></h1>
+            </div>
             <div className='border-2 bg-slate-400 py-2'>
                 <Banner isLoading= {isLoading} crafts = {data}></Banner>
             </div>
-
+            <div className='shadow-md rounded-md bg-[#93C572]  mb-0 mt-5  px-4 py-4 text-4xl'>
+                    <h1 className='py-4 border-b-4 text-white   border-b-white  pl-10'>Most Popular Cookwares <br /><small>A touch of elegance!</small></h1>
+            </div>
             <div className='grid md:grid-cols-2 gap-5  lg:grid-cols-3'>
                 {
                     isLoading? 
@@ -43,8 +48,13 @@ function Home() {
                 }
 
             </div>
+            
+            <div className='shadow-md rounded-md bg-[#FF5733]  mb-6 px-4 py-4 text-4xl'>
+                    <h1 className='py-4 border-b-4 text-white   border-b-white  pl-10'>Craft Categories <br /><small>you'd love to own!</small></h1>
+            </div>
+            
             <div className='flex justify-center'>
-                <button onClick={()=>setSeeAll(!seeAll)} className='btn btn-info text-white'>{seeAll?'See Less':'See All'}</button>
+                {data.length>=6 && <button onClick={()=>setSeeAll(!seeAll)} className='btn btn-info text-white'>{seeAll?'See Less':'See All'}</button>}
             </div>
         </div>
 
